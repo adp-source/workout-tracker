2f7d2403c1b9356f7889c125924d8fd8651febc8
Anhdung Pham <35167386+adp-source@users.noreply.github.com>
2018-03-15 03:41:19 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs290_phamanh',
-  password        : '7645',
-  database        : 'cs290_phamanh'
+  user            : 'cs290_secret',
+  password        : 'secret',
+  database        : 'cs290_secret'
 });
 
 module.exports.pool = pool;
